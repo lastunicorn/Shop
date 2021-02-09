b1f44a6c394d715e703623691028848395866023
Iuga Alexandru <coldblackstar@yahoo.com>
2021-02-09 09:31:22 +0200

    code cleanup

--- sources/Shop.WithRepository/WithRepository.Application/PresentOrders/PresentOrdersRequest.cs
+++ sources/Shop.WithRepository/WithRepository.Application/PresentOrders/PresentOrdersRequest.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using MediatR;
 using Shop.WithRepository.Domain;
-using Shop.WithRepository.Domain.DataAccess;
 
 namespace Shop.WithRepository.Application.PresentOrders
 {
     public class PresentOrdersRequest : IRequest<List<Order>>
     {
-    }
-
-    internal class PresentOrdersRequestHandler : IRequestHandler<PresentOrdersRequest, List<Order>>
-    {
-        private readonly IUnitOfWork unitOfWork;
-
-        public PresentOrdersRequestHandler(IUnitOfWork unitOfWork)
-        {
-            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
-        }
-
-        public Task<List<Order>> Handle(PresentOrdersRequest request, CancellationToken cancellationToken)
-        {
-            return Task.Run(() => unitOfWork.OrderRepository.GetAllFull(), cancellationToken);
-        }
     }
 }
